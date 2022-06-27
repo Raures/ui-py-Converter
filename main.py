0db712eb908073e4c161f2e2eb88bd4ed643fe47
Master <raures778@gmail.com>
2022-06-27 19:01:55 +0300

    Clean-up, removed pathlib

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
 import os
 import sys
 
-from pathlib import Path
-from widget.widget import MainWindow
+from widgets.main_window import MainWindow
 from PyQt5.QtWidgets import QApplication
 
-# Variable that holds the path where the .py file will be placed
-savePath = ""
+save_path = ""
+SETTINGS_PATH = "settings.txt"
 
-# Variable that holds the path/name of the settings file
-settingsFilePath = Path("settings.txt")
+if not SETTINGS_PATH.path.exists():
 
-# Check if settingsFilePath is present
-if not settingsFilePath.is_file():
-    
-    # If it isn't, create it
-    f = open("settings.txt", "w")
+    save_path = os.environ["HOMEPATH"] + "\\Desktop\\"
 
-    # Set the savePath to default
-    savePath = os.environ["HOMEPATH"] + "\\Desktop\\"
-    f.write(savePath)
-    
-    # Very important to close opened files
-    f.close()
+    with open("settings.txt", "w") as f:
+        f.write(save_path)
+
 else:
-    # If it is, open it for reading
-    f = open("settings.txt", "r")
-
-    # Read the savePath from settingsFilePath
-    savePath = f.readline()
-    
-    # Very important to close opened files
-    f.close()
-
+    with open("settings.txt", "r") as f:
+        save_path = f.readline()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ui = MainWindow(savePath, settingsFilePath)
+    ui = MainWindow(save_path, SETTINGS_PATH)
     sys.exit(app.exec_())
